83f631504a65495cd86f91939275aca6a7b41d8c
ma0 <ma0@contraslash.com>
2016-02-12 00:18:27 -0500

    Add Base Slug Field and gitignore

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils.text import slugify
+from utils import utils
 # Create your models here.
 class BaseModel(models.Model):
 
@@ -40,3 +41,23 @@ class FullBaseModel(BaseModel):
 
     class Meta:
         abstract = True
+
+
+class FullSlugBaseModel(BaseModel):
+    name = models.CharField(max_length=30, blank=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(blank=True, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            if self.objects.filter(slug=self.slug):
+                self.slug = self.slug + "-" + utils.generate_random_string(4)
+        return super(FullSlugBaseModel, self).save(force_insert, force_update, using, update_fields)
