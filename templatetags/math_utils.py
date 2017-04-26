749c4d7b8fdca85a82528a7776f377c093296697
Mauricio Collazos <ma0@contraslash.com>
2017-04-25 23:21:50 -0500

    added templatetags math_utils

--- templatetags/math_utils.py
+++ templatetags/math_utils.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name="abs")
 def abs_value(value):
     return abs(value)
+
+
+@register.filter(name="is_negative")
+def is_negative(value):
+    return value < 0
